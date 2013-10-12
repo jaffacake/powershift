8d9cac81a945aea0cc66017a01a0b23b9f52ca31
Grant <redbullgivsyouwings@gmail.com>
2013-10-12 16:22:32 +0100

    Fixed issue when calling the api_key variable

--- weatherController.php
+++ weatherController.php
@@ -1,17 +1,25 @@
 <?php
 
-require_once 'libs/forecast.io.php';
+require_once 'libs/forecast.php';
 require_once 'weather.php';
 
 class WeatherController{
     
     private $weather;
     private $api_key = '590e67842b699d251fad2f60b4f9cdc5';
+    private $forecast;
+    private $forecastToday;
+    
     
     public function __construct() {
         $this->weather = new Weather();
+        $this->forecast = new ForecastIO($this->api_key);
     }
     
+    public function getTodaysForecast($lat,$lng){
+        $this->forecastToday = $this->forecast->getForecastToday($lat, $lng);
+        return $this->forecastToday;
+    }
 }
 
 ?>
