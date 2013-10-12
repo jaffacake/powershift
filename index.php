ba89dcc5d99e91b5cd567830859f5ac0b8e1ee71
Grant <redbullgivsyouwings@gmail.com>
2013-10-12 18:16:24 +0100

    Starting to create a basic output.

--- index.php
+++ index.php
@@ -7,10 +7,36 @@ require_once('weatherController.php');
     
     $array = $locationController->getLocation("b92 0pu");
     
-    print_r($weatherController->getWeeksForecast($array['lat'], $array['lng']));
+    $array2 = $weatherController->getWeeksForecast($array['lat'], $array['lng']);
     
-    print_r($array);
+    foreach($array2 as $data){
+        echo $data->getMaxTemperature();
+    }
       
     echo "LocationController created";
     
 ?>
+
+<table>
+
+    <tr>
+        <?php
+        $i = 1;
+        foreach($array2 as $data){
+            
+        ?>
+        <td>
+            
+            <div class="day">
+            <?=Date('l', strtotime("+".$i." days"))?>
+                
+            <?=$data->getSummary()?></div>
+            
+        </td>
+        <?
+            $i++;
+        
+        }
+        ?>
+    </tr>
+</table>
