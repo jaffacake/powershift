9f3ce1c36fac33f46d97d31d513dae2221587302
Grant <redbullgivsyouwings@gmail.com>
2013-10-12 19:08:00 +0100

    Added Jquery to ajax the controller when searching. Need to come up with a default.

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
-require_once('locationController.php');
-require_once('weatherController.php');
-
-    $locationController = new LocationController();
-    $weatherController = new weatherController();
-    
-    $array = $locationController->getLocation("b92 0pu");
-    
-    $array2 = $weatherController->getWeeksForecast($array['lat'], $array['lng']);
     
 ?>
 <link href="style.css" rel="stylesheet" type="text/css" />
-<table>
+<script src="js/jquery-1.10.2.min.js"></script>
+
+<script>
+    $(document).ready(function(){
+      $("#search").submit(function(event){
+          event.preventDefault();
+          $.ajax({
+            type: "POST",
+            url: "controller.php",
+            data: { location: $("#location").val()}
+          })
+            .done(function( data ) {
+                if ( console && console.log ) {
+                console.log( "Sample of data:", data );
+                }
+            });
+          
+      });
+    
+    });
+    
+</script>
+
+<form id="search" method="POST">
+<input id="location" name="location"/>
+<input id="submit" type="submit"/>
+</form>
+
+<table min-height="200px">
 
     <tr valign="top" align="center">
         <?php
@@ -22,10 +41,10 @@ require_once('weatherController.php');
         <td>
             
             <div class="forecast">
-                <div class="day"><?=Date('l', strtotime("+".$i." days"))?></div>
+                <div class="day"><h2><?=Date('l', strtotime("+".$i." days"))?></h2></div>
                 <div class="icon"><img src="images/weather/<?=$data->getIcon()?>.png" width="96px" height="96px"/></div>
-                <div class="maxTemp">Max Temp:<?=$data->getMaxTemperature()?></div>
-                <div class="minTemp">Min Temp:<?=$data->getMinTemperature()?></div>
+                <div class="maxTemp">Max Temp: <?=$data->getMaxTemperature()?></div>
+                <div class="minTemp">Min Temp: <?=$data->getMinTemperature()?></div>
                 <div class="summary"><?=$data->getSummary()?></div>
             </div>
             
