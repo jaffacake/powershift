7e45d227603ad0d32fe999a18b77194f84ac5091
Grant <redbullgivsyouwings@gmail.com>
2013-10-12 16:04:41 +0100

    Create Necessary variables relating to the data from the Api. Generated Getters and Setters

--- weather.php
+++ weather.php
@@ -1,7 +1,61 @@
 <?php
 
 class Weather{
+    private $temperature;
+    private $summary;
+    private $icon;
+    private $humidity;
+    private $pressure;
+    private $visibility;
     
     
+    public function getTemperature() {
+    return $this->temperature;
+    }
+
+    public function setTemperature($temperature) {
+    $this->temperature = $temperature;
+    }
+
+    public function getSummary() {
+    return $this->summary;
+    }
+
+    public function setSummary($summary) {
+    $this->summary = $summary;
+    }
+
+    public function getIcon() {
+    return $this->icon;
+    }
+
+    public function setIcon($icon) {
+    $this->icon = $icon;
+    }
+
+    public function getHumidity() {
+    return $this->humidity;
+    }
+
+    public function setHumidity($humidity) {
+    $this->humidity = $humidity;
+    }
+
+    public function getPressure() {
+    return $this->pressure;
+    }
+
+    public function setPressure($pressure) {
+    $this->pressure = $pressure;
+    }
+
+    public function getVisibility() {
+    return $this->visibility;
+    }
+
+    public function setVisibility($visibility) {
+    $this->visibility = $visibility;
+    }
 }
+
 ?>
