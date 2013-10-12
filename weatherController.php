1e0c43f96c0f1e5bec95f065120fedb4fa204653
Grant <redbullgivsyouwings@gmail.com>
2013-10-12 15:58:10 +0100

    Created Constructor and necessary variables

--- weatherController.php
+++ weatherController.php
@@ -5,6 +5,13 @@ require_once 'weather.php';
 
 class WeatherController{
     
+    private $weather;
+    private $api_key = '590e67842b699d251fad2f60b4f9cdc5';
+    
+    public function __construct() {
+        $this->weather = new Weather();
+    }
     
 }
+
 ?>
