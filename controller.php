973a22e2cc4aa9be4c3121bf27ca9f05c54a8233
Grant <redbullgivsyouwings@gmail.com>
2013-10-13 10:40:41 +0100

    Expanded controller to handle day forecasts

--- controller.php
+++ controller.php
@@ -6,8 +6,10 @@ require_once('weatherController.php');
     $locationController = new LocationController();
     $weatherController = new weatherController();
 
-    if(isset($_POST['location'])){
+    if(isset($_POST['location'])&&isset($_POST['forecast'])){
         $array = $locationController->getLocation($_POST['location']);
+        if($_POST['forecast']=="week"){
+            
         $array2 = $weatherController->getWeeksForecast($array['lat'], $array['lng']);
         ?>
             <table min-height="200px">
@@ -22,7 +24,7 @@ require_once('weatherController.php');
             ?>
             <td>
 
-                <div class="forecast">
+                <div class="forecast" id="forecast<?=$i?>">
                     <div class="day" id="day<?=$i?>"><h2><?=Date('l', strtotime("+".$i." days"))?></h2></div>
                     <div class="icon" id="icon<?=$i?>"><img src="images/weather/<?=$data->getIcon()?>.png" width="96px" height="96px"/></div>
                     <div class="maxTemp" id="maxTemp<?=$i?>">Max Temp: <?=$data->getMaxTemperature()?></div>
@@ -39,6 +41,30 @@ require_once('weatherController.php');
         </tr>
     </table>
 <?php
+        }
+        elseif ($_POST['forecast']=="day"){
+            if($_POST['forecastDay']=="day1"){
+                $array2 = $weatherController->getTodaysForecast($array['lat'], $array['lng']);
+            }
+            else{
+                preg_match('!\d+!', $_POST['forecastDay'], $match);
+                $timestamp = date_timestamp_get(Date('l', strtotime("+".$match[0]." days")));
+                $array2 = $weatherController->getdayForecast($array['lat'], $array['lng'], $timestamp);
+            }
+            ?>
+
+            <div><?php
+            $i = 1;
+            foreach($array2 as $data){
+                echo $data->getMaxTemperature();
+            ?> 
+            
+            <?
+            }
+            ?>
+                </div>
+            <?
+        }
     }else{
         echo "null";
     }
