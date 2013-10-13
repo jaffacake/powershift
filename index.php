239c8cbaf41c115ca464bce988d4078966407238
Grant <redbullgivsyouwings@gmail.com>
2013-10-13 12:51:16 +0100

    Cleaned up so that the results are AJAX'ed for and not shown on load. Allowing the user to choose a location

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php
-    require_once('locationController.php');
-    require_once('weatherController.php');
 
-    $locationController = new LocationController();
-    $weatherController = new weatherController();
-    
-    $array = $locationController->getLocation($_SERVER['REMOTE_ADDR']);
-    $array2 = $weatherController->getWeeksForecast($array['lat'], $array['lng']);
-?>
 <link href="style.css" rel="stylesheet" type="text/css" />
 <script src="js/jquery-1.10.2.min.js"></script>
 
@@ -48,9 +39,24 @@
                 $("#LoadingImage").hide();
                 $("#day-summary").html(data);
             }
-          });
-            
+          }); 
       });
+      
+      $('body').on("click", '#currentLocation', function ( event ) {
+        $("#LoadingImage").show();
+        $("#day-summary").html("");
+        event.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "controller.php",
+          data: { forecast: "week", location: "<?=$_SERVER['REMOTE_ADDR']?>"},
+          success: function( data ) {
+              $("#LoadingImage").hide();
+              $("#content").html(data);
+          }
+        });
+
+    });
     
     });
     
@@ -59,41 +65,14 @@
 <form id="search" method="POST">
     <input id="location" name="location"/>
     <input id="submit" type="submit"/>
-    <input type="button" name="currentLocation" value="Use Current Location"/>
+    <input type="button" id="currentLocation" name="currentLocation" value="Use Current Location"/>
     <div id="LoadingImage" style="display: none">
         <img src="images/ajax_loading.gif" />
     </div>
 </form>
 
 <div id="content">
-    <table min-height="200px">
-        <tr>
-            <td><h1 id="locationName"><?=$array['name']?></h1></td>
-        </tr>
-        <tr valign="top" align="center">
-            <?php
-            $i = 1;
-            foreach($array2 as $data){
-
-            ?>
-            <td>
-
-                <div class="forecast" id="forecast<?=$i?>">
-                    <div class="day" id="day<?=$i?>"><h2><?=Date('l', strtotime("+".$i." days"))?></h2></div>
-                    <div class="icon" id="icon<?=$i?>"><img src="images/weather/<?=$data->getIcon()?>.png" width="96px" height="96px"/></div>
-                    <div class="maxTemp" id="maxTemp<?=$i?>">Max Temp: <?=$data->getMaxTemperature()?>&deg;</div>
-                    <div class="minTemp" id="minTemp<?=$i?>">Min Temp: <?=$data->getMinTemperature()?>&deg;</div>
-                    <div class="summary" id="summary<?=$i?>"><?=$data->getSummary()?></div>
-                </div>
-
-            </td>
-            <?php
-                $i++;
-
-            }
-            ?>
-        </tr>
-    </table>
+    
 </div>
 
-<div id="day-summary"></div>
+<div id="day-summary" class="day-summary"></div>
